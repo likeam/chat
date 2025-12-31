9632ef940d24bcfbd141a816e6d80f2a74b3bdc4
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2025-12-31 22:59:20 +0500

    Removing Error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,7 +31,7 @@ const Home = () => {
   return (
     <main className=" flex min-h-screen flex-col items-center justify-center p-4">
       <div className=" w-full max-w-md space-y-8">
-        <div className=" text-center space-y-9">
+        <div className=" text-center space-y-8">
           <h1 className=" text-2xl font-bold tracking-tight text-green-500">
             {">"}private_chat
           </h1>
